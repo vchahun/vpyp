0e1febba285537c96d577b4acf55d2fd5a3c4dc2
Victor Chahuneau <vchahune@cs.cmu.edu>
2013-02-19 19:19:01 -0500

    Improve vocabulary: add START/STOP symbols option, initialization

--- vpyp/corpus.py
+++ vpyp/corpus.py
@@ -6,20 +6,37 @@ START, STOP = 0, 1
 class OOV(Exception): pass
 
 class Vocabulary:
-    def __init__(self):
-        self.word2id = {'<s>': START, '</s>': STOP}
-        self.id2word = ['<s>', '</s>']
+    def __init__(self, start_stop=True, init=None):
+        if start_stop:
+            self.word2id = {'<s>': START, '</s>': STOP}
+            self.id2word = ['<s>', '</s>']
+        else:
+            self.word2id = {}
+            self.id2word = []
         self.frozen = False
+        if init is not None:
+            for word in init:
+                self[word]
 
     def __getitem__(self, word):
         if isinstance(word, int):
             assert word >= 0
             return self.id2word[word]
         if word not in self.word2id:
-            if hasattr(self, 'frozen') and self.frozen: raise OOV(word)
+            if self.frozen: raise OOV(word)
             self.word2id[word] = len(self)
             self.id2word.append(word)
         return self.word2id[word]
+
+    def __setitem__(self, word, value):
+        if isinstance(word, int):
+            assert 0 <= word < len(self)
+            self.id2word[word] = value
+            self.word2id[value] = word
+        else:
+            assert 0 <= value < len(self)
+            self.id2word[value] = word
+            self.word2id[word] = value
 
     def update(self, vocabulary):
         self.word2id = vocabulary.word2id
